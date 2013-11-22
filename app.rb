70ae85c23ab5ed14572cf466407315f9403e0757
eli shkurkin <eshkurkin@gmail.com>
2013-11-22 10:20:23 -0800

    get flash working

--- app.rb
+++ app.rb
@@ -3,9 +3,14 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require './environments'
+require 'sinatra/flash'
+require 'sinatra/redirect_with_flash'
+
+enable :sessions
 
 class Post < ActiveRecord::Base
-
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :body, presence: true
 end
 
 get "/" do
@@ -23,3 +28,27 @@ helpers do
     end
   end
 end
+
+get "/posts/create" do
+  @title = "Create Post"
+  @post = Post.new
+  erb :"posts/create"
+end
+
+post "/posts" do
+  @post = Post.new(params[:post])
+  if @post.save
+    redirect "posts/#{@post.id}", :notice => 'Swell post.'
+  else
+    redirect :"posts/create", :error => 'Error'
+  end
+end
+
+get "/posts/:id" do
+  @post = Post.find(params[:id])
+  @title = @post.title
+  erb :"posts/view"  
+end
+
+
+
